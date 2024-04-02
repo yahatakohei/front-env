03ffec8e337ec0dfd779b31ad7714c2328325a18
kohei.yahata <kohei.yahata.pu@ambl.co.jp>
2024-04-02 15:39:29 +0900

    first commit

---
+++ src/components/pages/Test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Test = () => {
+  return (
+    <div>
+      <h1>Hello, Next.js!</h1>
+      <p>This is a simple example of a Next.js app.</p>
+    </div>
+  );
+};
